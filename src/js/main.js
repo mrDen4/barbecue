2461a6372ffdbc2924cdead78181de8de463ff53
Denis Davydov <den.saint.k@gmail.com>
2018-07-08 02:15:18 +0300

    finish main page

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,35 @@
 $(document).ready(() => {
-    $('.header__btn').on('click', function() {
-        $('.header__contacts').toggleClass('header__contacts--active');
-        $('.header__list').toggleClass('header__list--active');
-        console.log('click');
+    $('.header__humburger').on('click', function() {
+        $('.header__nav').toggleClass('header__nav--active');
+    });
+    
+    $('.popular__list').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: $('.popular__prev'),
+        nextArrow: $('.popular__next'),
+        responsive: [
+            {
+              breakpoint: 700,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+        ]
     });
 
-    $(function(){
-        $('.hero__slider').bxSlider({
-          mode: 'fade',
-          captions: true
-        });
-      });
+    $('.hero__slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: $('.hero__prev'),
+        nextArrow: $('.hero__next')
+    });
 });
